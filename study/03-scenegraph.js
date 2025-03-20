367e4cb167bd6e8138729bc6581cb40657ddf3df
jhon-rjf <ykjung0917@kakao.com>
2025-03-20 11:04:05 +0900

    03 - scenegraph

--- study/03-scenegraph.js
+++ study/03-scenegraph.js
@@ -1,8 +1,5 @@
 // 20201507 정윤걸 과제제출용 인증 주석
 import * as THREE from '../build/three.module.js';
-import { OrbitControls } from '../examples/jsm/controls/OrbitControls.js';
-import { FontLoader } from '../examples/jsm/loaders/FontLoader.js';
-import { TextGeometry } from '../examples/jsm/geometries/TextGeometry.js';
 
 class App {
     constructor() {
@@ -22,7 +19,6 @@ class App {
         this._setupCamera();
         this._setupLight();
         this._setupModel();
-        this._setupControls();
         
         window.onresize = this.resize.bind(this);
         this.resize();
@@ -34,7 +30,7 @@ class App {
         const width = this.divContainer.clientWidth;
         const height = this.divContainer.clientHeight;
         const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-        camera.position.z = 15;
+        camera.position.z = 14;
         this._camera = camera;
     }
 
@@ -47,45 +43,42 @@ class App {
     }
 
     _setupModel() {
-        const fontLoader = new FontLoader();
-        
-        async function loadFont(that) {
-            const url = "../examples/fonts/helvetiker_regular.typeface.json";
-            const font = await new Promise((resolve, reject) => {
-                fontLoader.load(url, resolve, undefined, reject);
-            });
-            
-            const geometry = new TextGeometry("JYG", {
-                font: font,
-                size: 5,
-                height: 0,
-                curveSegments: 4,
-                bevelEnabled: true,
-                bevelThickness: 0.7,
-                bevelSize: 0.7,
-                bevelSegments: 2
-            });
-            
-            const fillMaterial = new THREE.MeshPhongMaterial({color: 0x515151});
-            const cube = new THREE.Mesh(geometry, fillMaterial);
-            
-            const lineMaterial = new THREE.LineBasicMaterial({color: 0xffff00});
-            const line = new THREE.LineSegments(
-                new THREE.WireframeGeometry(geometry), lineMaterial);
-            
-            const group = new THREE.Group();
-            group.add(cube);
-            group.add(line);
-            
-            that._scene.add(group);
-        };
-        
-        loadFont(this);
-    }
+        const solarSystem = new THREE.Object3D();
+        this._scene.add(solarSystem);   
+        this._solarSystem = solarSystem;
 
-    _setupControls() {
-        this._controls = new OrbitControls(this._camera, this._renderer.domElement);
-        this._controls.enableDamping = true;
+        const radius = 1;
+        const widthSegments = 12;
+        const heightSegments = 12;
+        const sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+
+        const sunMaterial = new THREE.MeshPhongMaterial({emissive: 0xffff00, flatShading: true});
+
+        const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial);
+        sunMesh.scale.set(3, 3, 3);
+        solarSystem.add(sunMesh);
+
+        const earthOrbit = new THREE.Object3D();
+        solarSystem.add(earthOrbit);
+
+        const earthMaterial = new THREE.MeshPhongMaterial({color: 0x2233ff, emissive: 0x112244, flatShading: true});
+
+       const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial);
+       earthOrbit.position.x = 10;
+       earthOrbit.add(earthMesh);
+
+       const moonOrbit = new THREE.Object3D();
+       moonOrbit.position.x = 2;
+       earthOrbit.add(moonOrbit);
+
+       const moonMaterial = new THREE.MeshPhongMaterial({color: 0x888888, emissive: 0x222222, flatShading: true});
+       const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial);
+       moonMesh.scale.set(0.5, 0.5, 0.5);
+       moonOrbit.add(moonMesh);
+
+       this._solarSystem = solarSystem;
+       this._earthOrbit = earthOrbit;
+       this._moonOrbit = moonOrbit;
     }
 
     resize() {
@@ -103,7 +96,10 @@ class App {
     }
 
     update(time) {
-        this._controls.update();
+        time *= 0.001;
+        this._solarSystem.rotation.y = time/2;
+        this._earthOrbit.rotation.y = time * 2;
+        this._moonOrbit.rotation.y = time * 5;
     }
 }
 
